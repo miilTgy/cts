1324d1c9539c105e429eebf38774cc12bc0a9431
miilTgy <142780211+miilTgy@users.noreply.github.com>
2026-05-09 14:57:26 +0800

    Connect all into main flow.

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,9 @@
 #include "common.h"
 #include "parser.h"
 #include "treer.h"
+#include "bu.h"
+#include "td.h"
+#include "writer.h"
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -12,6 +15,9 @@ int main(int argc, char** argv) {
 
     parser::debug_enable(true);
     treer::debug_enable(true);
+    bu::debug_enable(true);
+    td::debug_enable(true);
+    writer::debug_enable(true);
 
     common::Problem problem = parser::parse(argv[1]);
     if (!problem.valid) {
@@ -24,6 +30,30 @@ int main(int argc, char** argv) {
         std::cerr << "Treer error: " << tree.error_msg << "\n";
         return 1;
     }
+
+    // Temporarily stop after topology generation. Re-enable the following
+    // stages when BU/TD/Writer are ready to be run in the full CTS flow.
+    
+    common::BottomUpResult bu_result = bu::run(problem, tree);
+    if (!bu_result.valid) {
+        std::cerr << "BU error: " << bu_result.error_msg << "\n";
+        return 1;
+    }
+    
+    common::TopDownResult td_result = td::run(problem, tree, bu_result);
+    if (!td_result.valid) {
+        std::cerr << "TD error: " << td_result.error_msg << "\n";
+        return 1;
+    }
+    
+    writer::WriterResult writer_result =
+        writer::write_solution(argv[1], problem, tree, bu_result, td_result);
+    if (!writer_result.valid) {
+        std::cerr << "Writer error: " << writer_result.error_msg << "\n";
+        return 1;
+    }
+    
+    std::cout << "Wrote solution: " << writer_result.output_path << "\n";
 
     return 0;
 }
