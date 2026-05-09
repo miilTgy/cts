c7bd10386e61b6483c4d35bcb53e54fb51e64d64
miilTgy <142780211+miilTgy@users.noreply.github.com>
2026-05-09 14:55:45 +0800

    Add runall and evall scripts.

---
+++ evall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+make eval SAMPLE=samples/sample1.txt
+make eval SAMPLE=samples/sample2.txt
+make eval SAMPLE=samples/sample3.txt
---
+++ runall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+make run SAMPLE=samples/sample1.txt
+make run SAMPLE=samples/sample2.txt
+make run SAMPLE=samples/sample3.txt
